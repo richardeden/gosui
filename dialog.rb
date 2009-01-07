40e295092a18ac98a8e51c4fd0968dbb27d1e82c
Richard Patching <richard@edendevelopment.co.uk>
2009-01-07 13:36:00 +0000

    Updated dialog box

--- dialog.rb
+++ dialog.rb
@@ -1,5 +1,7 @@
 module Gosui
   class Dialog
+    
+    attr_accessor :dialog_title_box, :dialog_box
     def initialize(window, size_x, size_y, loc_x, loc_y, text)
       @window = window
       @size_x = size_x
@@ -7,16 +9,28 @@ module Gosui
       @loc_x = loc_x
       @loc_y = loc_y
       @text = text
-      img = File.expand_path(File.dirname(__FILE__) + '/media/dialog.png')
-      dialog_img = Magick::Image.read(img).first
-      dialog_img_scaled = dialog_img.scale(size_x, size_y)
+      
+      setup_image(window, size_x, size_y)
+      
+      @font = Gosu::Font.new(@window, Gosu::default_font_name, 12)
+    end
+    
+    def setup_image(window, size_x, size_y)
+      dialog_title_img = dialog_box_img = File.expand_path(File.dirname(__FILE__) + '/media/dialog_title_bar.png')
+      m_dialog_title_img = Magick::Image.read(dialog_title_img).first
+      dialog_title_img_scaled = m_dialog_title_img.scale(size_x, 25)
+      @dialog_title_box = Gosu::Image.new(window, dialog_title_img_scaled, true)
+      
+      dialog_box_img = File.expand_path(File.dirname(__FILE__) + '/media/dialog.png')
+      m_dialog_img = Magick::Image.read(dialog_box_img).first
+      dialog_img_scaled = m_dialog_img.scale(size_x, size_y)
       @dialog_box = Gosu::Image.new(window, dialog_img_scaled, true)
-      @font = Gosu::Font.new(@window, Gosu::default_font_name, 20)
     end
     
     def draw
-      @dialog_box.draw(@loc_x, @loc_y, Gosui::ZOrder::Dialog)
-      @font.draw("#{@text}",@loc_x + 30, @loc_y, Gosui::ZOrder::Dialog, 1.0, 1.0, 0xffffff00)
+      @dialog_title_box.draw(@loc_x, @loc_y, Gosui::ZOrder::Dialog)
+      @dialog_box.draw(@loc_x, @loc_y+25, Gosui::ZOrder::Dialog)
+      @font.draw("#{@text}",@loc_x + 100, @loc_y + 10, Gosui::ZOrder::Dialog, 1.0, 1.0, 0xffffffff)
     end
   end
 end
